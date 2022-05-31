09ffc8711207c139a44d066029a9074d550c4d5b
thegamerx1 <34986564+thegamerx1@users.noreply.github.com>
2022-05-31 22:07:01 +0200

    fix domain

--- src/handler.js
+++ src/handler.js
@@ -8,6 +8,18 @@ app.get('/healthcheck', (req, res) => {
 	res.end('ok');
 });
 
+app.use((req, res, next) => {
+	let subdomains = req.subdomains.reverse().join('.');
+	let host = req.hostname.replace(subdomains + '.', '');
+
+	if (subdomains === 'i' || !subdomains) {
+		next();
+	} else {
+		console.log(`${req.protocol}://i.${host}${req.url}`);
+		res.redirect(`${req.protocol}://i.${host}${req.url}`);
+	}
+});
+
 // let SvelteKit handle everything else, including serving prerendered pages and static assets
 app.use(handler);
 
